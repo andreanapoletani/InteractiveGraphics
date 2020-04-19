23fbd75542cc3f37567518117f89bb68cacafdc7
Andrea Napoletani <andrea.napoletani@gmail.com>
2020-04-19 16:21:44 +0200

    Light computation in fragment shader
    Changed the position of light computations from the vertex to the fragment shaders.

--- homework1/test.js
+++ homework1/test.js
@@ -1,36 +1,41 @@
+var homework1 = function() {
+
 "use strict";
 
-var homework1 = function() {
 var canvas;
 var gl;
 
-
+var program;
 var index = 0;
+
+var c;
 
 var positionsArray = [];
 var normalsArray = [];
-
-
-var near = 1.0;
-var far = 9.0;
-var radius = 3.4;
-var theta = 0.52;
-var phi = -0.785;
-var dr = 5.0 * Math.PI/180.0;
-
-var  fovy = 70.0;  // Field-of-view in Y direction angle (in degrees)
-var  aspect = 1.0;       // Viewport aspect ratio
 
 var va = vec4(0.0, 0.0, -1.0,1);
 var vb = vec4(0.0, 0.942809, 0.333333, 1);
 var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
 var vd = vec4(0.816497, -0.471405, 0.333333,1);
 
+var near = 1.0;
+var far = 9.0;
+var radius = 3.4;
+var theta = 0.6;
+var phi = 0.6;
+var dr = 5.0 * Math.PI/180.0;
+
+var  fovy = 45.0;  // Field-of-view in Y direction angle (in degrees)
+var  aspect = 1.0;       // Viewport aspect ratio
+
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
 var eye;
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
 
-var lightPosition = vec4(10.0, 30.0, 0.0, 0.0);
+//var lightPosition = vec4(-10.0, -10.0, 10.0, 0.0);
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
 var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
 var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
 var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
@@ -40,20 +45,22 @@ var materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
 var materialSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 var materialShininess = 20.0;
 
-var ctm;
+var spotLightPosition = vec4(10.0, 10.0, 10.0, 1.0 );
+var spotLightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var spotLightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var spotLightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var spotLightDirection = vec4(-0.5,1.0,2.0,1.0);
+var lCutOff=-1;
+var spotAlhpa = 1;
+
 var ambientColor, diffuseColor, specularColor;
-
-var modelViewMatrix, projectionMatrix;
-var modelViewMatrixLoc, projectionMatrixLoc;
-var nMatrix, nMatrixLoc;
-
 
 function triangle(a, b, c) {
 
      var t1 = subtract(b, a);
      var t2 = subtract(c, a);
      var normal = normalize(cross(t2, t1));
-     normal = vec4(normal[0], normal[1], normal[2], 0.0);
+     normal = vec3(normal[0], normal[1], normal[2]);
 
      normalsArray.push(normal);
      normalsArray.push(normal);
@@ -95,30 +102,24 @@ function tetrahedron(a, b, c, d, n) {
     divideTriangle(a, c, d, n);
 }
 
+
 window.onload = function init() {
 
-    canvas = document.getElementById("gl-canvas");
+    canvas = document.getElementById( "gl-canvas" );
 
     gl = canvas.getContext('webgl2');
-    if (!gl) alert("WebGL 2.0 isn't available");
+    if (!gl) alert("WebGL 2.0 isn't available" );
 
-
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
     gl.enable(gl.DEPTH_TEST);
 
     //
     //  Load shaders and initialize attribute buffers
     //
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
-
-
-    var ambientProduct = mult(lightAmbient, materialAmbient);
-    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
-    var specularProduct = mult(lightSpecular, materialSpecular);
-
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
 
     tetrahedron(va, vb, vc, vd, 2);
 
@@ -127,21 +128,27 @@ window.onload = function init() {
     gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
 
     var normalLoc = gl.getAttribLocation(program, "aNormal");
-    gl.vertexAttribPointer(normalLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(normalLoc);
 
-
     var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
 
-    var positionLoc = gl.getAttribLocation( program, "aPosition");
-    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLoc);
+    var vPosition = gl.getAttribLocation( program, "aPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
 
     modelViewMatrixLoc = gl.getUniformLocation(program, "uModelViewMatrix");
     projectionMatrixLoc = gl.getUniformLocation(program, "uProjectionMatrix");
-    nMatrixLoc = gl.getUniformLocation(program, "uNormalMatrix");
+
+    var ambientProduct = mult(lightAmbient, materialAmbient);
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
+
+    var spotAmbientProduct = mult(spotLightAmbient, materialAmbient);
+    var spotDiffuseProduct = mult(spotLightDiffuse, materialDiffuse);
+    var spotSpecularProduct = mult(spotLightSpecular, materialSpecular);
 
     // sliders for viewing parameters
 
@@ -174,41 +181,50 @@ window.onload = function init() {
         console.log("fovy " + fovy);
     };
 
+    gl.uniform4fv(gl.getUniformLocation(program, "uAmbientProduct"),
+       ambientProduct);
+    gl.uniform4fv(gl.getUniformLocation(program, "uDiffuseProduct"),
+       diffuseProduct );
+    gl.uniform4fv(gl.getUniformLocation(program, "uSpecularProduct"),
+       specularProduct );
+    gl.uniform4fv(gl.getUniformLocation(program, "uLightPosition"),
+       lightPosition );
 
     gl.uniform4fv( gl.getUniformLocation(program,
-       "uAmbientProduct"),flatten(ambientProduct));
+      "spotAmbientProduct"),flatten(spotAmbientProduct) );
     gl.uniform4fv( gl.getUniformLocation(program,
-       "uDiffuseProduct"),flatten(diffuseProduct));
+      "spotDiffuseProduct"),flatten(spotDiffuseProduct) );
     gl.uniform4fv( gl.getUniformLocation(program,
-       "uSpecularProduct"),flatten(specularProduct));
+      "spotSpecularProduct"),flatten(spotSpecularProduct) );
     gl.uniform4fv( gl.getUniformLocation(program,
-       "uLightPosition"),flatten(lightPosition));
+       "spotLightPosition"),flatten(spotLightPosition) );
     gl.uniform1f( gl.getUniformLocation(program,
-       "uShininess"),materialShininess);
+       "lCutOff"),lCutOff );
+    gl.uniform1f( gl.getUniformLocation(program,
+          "spotAlpha"),spotAlhpa );
+    gl.uniform4fv( gl.getUniformLocation(program,
+       "spotLightDirection"),spotLightDirection );
+
+    gl.uniform1f(gl.getUniformLocation(program,
+       "uShininess"), materialShininess);
 
     render();
 }
 
-
-function render() {
-
+var render = function() {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     eye = vec3(radius*Math.sin(theta)*Math.cos(phi),
-        radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
-
-    modelViewMatrix = lookAt(eye, at , up);
+          radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
+    modelViewMatrix = lookAt(eye, at, up);
     projectionMatrix = perspective(fovy, aspect, near, far);
-    nMatrix = normalMatrix(modelViewMatrix, true);
-
 
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
-    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
     gl.uniformMatrix3fv(nMatrixLoc, false, flatten(nMatrix)  );
 
     for( var i=0; i<index; i+=3)
         gl.drawArrays(gl.TRIANGLES, i, 3);
-
 
     requestAnimationFrame(render);
 }
