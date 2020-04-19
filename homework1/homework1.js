c62f2dac52bd22a31691f5893cc01fbb6b8f0ed2
Andrea Napoletani <andrea.napoletani@gmail.com>
2020-04-20 01:05:10 +0200

    Spotlight added
    Done:
    - point 1 (to check)
    - point 2 (ok)
    - point 3 (ok)
    - point4 (ok)

--- homework1/homework1.js
+++ homework1/homework1.js
@@ -11,8 +11,8 @@ var positionsArray = [];
 var normalsArray = [];
 
 
-var near = 1.0;
-var far = 9.0;
+var near = 2.0;
+var far = 5.0;
 var radius = 3.4;
 var theta = 0.52;
 var phi = -0.785;
@@ -30,7 +30,7 @@ var eye;
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
 
-var lightPosition = vec4(10.0, 30.0, 0.0, 0.0);
+var lightPosition = vec4(-40.0, 50.0, 0.0, 0.0);
 var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
 var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
 var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
@@ -39,6 +39,15 @@ var materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
 var materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
 var materialSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 var materialShininess = 20.0;
+
+var spotLightPosition = vec4(-10.0, -10.0, 10.0, 1.0 );
+var spotLightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var spotLightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var spotLightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var spotLightDirection = vec4(-0.5,1.0,2.0,1.0);
+var spotTheta=0.43;
+var spotAlhpa = 1;
+var spotThetaVal, spotAlphaVal;
 
 var ctm;
 var ambientColor, diffuseColor, specularColor;
@@ -104,7 +113,7 @@ window.onload = function init() {
 
 
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     gl.enable(gl.DEPTH_TEST);
 
@@ -118,6 +127,10 @@ window.onload = function init() {
     var ambientProduct = mult(lightAmbient, materialAmbient);
     var diffuseProduct = mult(lightDiffuse, materialDiffuse);
     var specularProduct = mult(lightSpecular, materialSpecular);
+
+    var spotAmbientProduct = mult(spotLightAmbient, materialAmbient);
+    var spotDiffuseProduct = mult(spotLightDiffuse, materialDiffuse);
+    var spotSpecularProduct = mult(spotLightSpecular, materialSpecular);
 
 
     tetrahedron(va, vb, vc, vd, 2);
@@ -145,34 +158,49 @@ window.onload = function init() {
 
     // sliders for viewing parameters
 
-    document.getElementById("zFarSlider").onchange = function(event) {
-        far = event.target.value;
-        console.log("far " + far);
+    document.getElementById("zNearSlider").oninput = function (event) {
+        if(this.valueAsNumber !== far){
+            near = this.valueAsNumber;
+            console.log("near: " + near);
+        }
     };
-    document.getElementById("zNearSlider").onchange = function(event) {
-        near = event.target.value;
-        console.log("near " + near);
+    document.getElementById("zFarSlider").oninput = function (event) {
+        if(this.valueAsNumber !== near){
+            far = this.valueAsNumber;
+            console.log("far: " + far);
+        }
     };
-    document.getElementById("radiusSlider").onchange = function(event) {
-       radius = event.target.value;
-       console.log("radius " + radius);
+    document.getElementById("radiusSlider").oninput = function(event) {
+        if(this.valueAsNumber !== radius){
+            radius = this.valueAsNumber;
+            console.log("radius: " + radius);
+        }
     };
-    document.getElementById("thetaSlider").onchange = function(event) {
-        theta = event.target.value* Math.PI/180.0;
+    document.getElementById("thetaSlider").oninput = function(event) {
+      if(this.valueAsNumber !== theta){
+        theta = this.valueAsNumber* Math.PI/180.0;
         console.log("theta " + theta);
+      }
     };
-    document.getElementById("phiSlider").onchange = function(event) {
-        phi = event.target.value* Math.PI/180.0;
+    document.getElementById("phiSlider").oninput = function(event) {
+      if(this.valueAsNumber !== phi){
+        phi = this.valueAsNumber* Math.PI/180.0;
         console.log("phi " + phi);
+      }
     };
-    document.getElementById("aspectSlider").onchange = function(event) {
-        aspect = event.target.value;
+    document.getElementById("aspectSlider").oninput = function(event) {
+      if(this.valueAsNumber !== aspect){
+        aspect = this.valueAsNumber;
         console.log("aspect " + aspect);
+      }
     };
-    document.getElementById("fovSlider").onchange = function(event) {
-        fovy = event.target.value;
-        console.log("fovy " + fovy);
+    document.getElementById("fovSlider").oninput = function(event) {
+      if(this.valueAsNumber !== fovy){
+        fovy = this.valueAsNumber;
+        console.log("fov " + fovy);
+      }
     };
+
 
 
     gl.uniform4fv( gl.getUniformLocation(program,
@@ -186,6 +214,22 @@ window.onload = function init() {
     gl.uniform1f( gl.getUniformLocation(program,
        "uShininess"),materialShininess);
 
+     gl.uniform4fv( gl.getUniformLocation(program,
+       "spotAmbientProduct"),flatten(spotAmbientProduct) );
+     gl.uniform4fv( gl.getUniformLocation(program,
+       "spotDiffuseProduct"),flatten(spotDiffuseProduct) );
+     gl.uniform4fv( gl.getUniformLocation(program,
+       "spotSpecularProduct"),flatten(spotSpecularProduct) );
+     gl.uniform4fv( gl.getUniformLocation(program,
+        "spotLightPosition"),flatten(spotLightPosition) );
+     gl.uniform4fv( gl.getUniformLocation(program,
+        "spotLightDirection"),spotLightDirection );
+      //gl.uniform1f( gl.getUniformLocation(program,
+      //  "lCutOff"),lCutOff);
+      //gl.uniform1f( gl.getUniformLocation(program,
+      //  "spotAlpha"),spotAlhpa);
+      spotThetaVal  = gl.getUniformLocation(program, "spotTheta");
+      spotAlphaVal = gl.getUniformLocation(program, "spotAlhpa");
     render();
 }
 
@@ -205,6 +249,25 @@ function render() {
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
     gl.uniformMatrix3fv(nMatrixLoc, false, flatten(nMatrix)  );
+
+    document.getElementById("spotThetaSlider").oninput = function(event) {
+      if(this.valueAsNumber !== spotTheta){
+        spotTheta = this.valueAsNumber;
+        console.log("Spotlight theta value " + spotTheta);
+      }
+    };
+
+    document.getElementById("alphaslider").oninput = function(event) {
+      if(this.valueAsNumber !== spotAlhpa){
+        spotAlhpa = this.valueAsNumber;
+        console.log("Spotlight alpha value " + spotAlhpa);
+      }
+    };
+
+    gl.uniform1f(spotThetaVal, spotTheta);
+    gl.uniform1f(spotAlphaVal, spotAlhpa);
+
+    console.log("new spotlight theta value: " + spotTheta);
 
     for( var i=0; i<index; i+=3)
         gl.drawArrays(gl.TRIANGLES, i, 3);
